51555de483702eb63b70c4d0525095401d52cd28
Dobby <107937920+SimplyNorant@users.noreply.github.com>
2026-04-14 09:39:07 +0300

    DB system creation in NoteSystem

--- src/components/Pages/NoteSystem.tsx
+++ src/components/Pages/NoteSystem.tsx
@@ -6,10 +6,14 @@ import type { Session } from "@supabase/supabase-js";
 import { li } from "motion/react-client";
 
 interface Note {
-  id?: number;
+  id: number;
   title: string;
   description: string;
   created_at?: string;
+}
+
+interface NoteProp extends Note {
+  onDelete: Function;
 }
 
 export default function NoteSystem() {
@@ -57,6 +61,17 @@ export default function NoteSystem() {
     }
   };
 
+  const deleteTask = async (id: number) => {
+    setNotes((prev) => prev.filter((t) => t.id !== id));
+
+    // Server delete
+    const { error } = await supabase.from("notes").delete().eq("id", id);
+
+    if (error) {
+      console.error("Delete failed: ", error);
+    }
+  };
+
   return (
     <>
       <div className="ml-5 mt-3 text-2xl">
@@ -68,7 +83,12 @@ export default function NoteSystem() {
         <form className="flex items-start gap-2">
           <div className="flex flex-col">
             <label htmlFor="title">Title</label>
-            <input type="text" className="border" id="title" />
+            <input
+              type="text"
+              onInput={(e: any) => setNoteTitle(e.target.value)}
+              className="border"
+              id="title"
+            />
           </div>
           <div className="flex flex-col">
             <label htmlFor="description">Description</label>
@@ -89,8 +109,13 @@ export default function NoteSystem() {
         </form>
         <ul className="list-disc pl-5">
           {notes.map((el) => (
-            <li>
-              <Note title={el.title} description={el.description} key={el.id} />
+            <li key={el.id}>
+              <Note
+                id={el.id}
+                title={el.title}
+                description={el.description}
+                onDelete={deleteTask}
+              />
             </li>
           ))}
         </ul>
@@ -99,12 +124,31 @@ export default function NoteSystem() {
   );
 }
 
-function Note({ title, description }: Note) {
+function Note({ id, title, description, onDelete }: NoteProp) {
   return (
     <>
       <div>
         <span className="text-3xl font-semibold">{title}</span> -{" "}
         <span>{description}</span>
+        <button
+          onClick={() => onDelete(id)}
+          className="text-delete hover:text-red-800 transition "
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="size-8"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18 18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
       </div>
     </>
   );
